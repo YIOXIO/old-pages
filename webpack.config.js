f712f94a55afeb1a1bd41a02de7e0c99407d288f
unknown <C:\The Bat!>
2024-08-19 11:52:40 +0300

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const pages = [
+    { name: 'index', path: './src/index.html' },
+    { name: 'fgbou', path: './src/pages/fgbou.html' },
+    {name: 'xxxvi', path: './src/pages/xxxvi.html'},
+    {name: 'student-table', path: './src/pages/student-table.html'},
+    {name: 'scholarship', path: './src/pages/scholarship.html'},
+    {name: 'e-resources', path: './src/pages/e-resources.html'},
+    {name: 'graduade', path: './src/pages/graduade.html'},
+    {name: 'step-science', path: './src/pages/step-science.html'},
+    {name: 'actual-block', path: './src/pages/actual-block.html'},
+
+    
+];
+
 module.exports = {
     entry: {
         main: './src/index.js'
@@ -12,13 +26,11 @@ module.exports = {
         filename: 'main.js',
         publicPath: ''
     },
-
     mode: 'development',
     devServer: {
         static: path.resolve(__dirname, './dist'),
         compress: true,
         port: 8080,
-
         open: true
     },
     module: {
@@ -46,12 +58,11 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin(),
+        ...pages.map(page => new HtmlWebpackPlugin({
+            filename: page.name + '.html',
+            template: page.path
+        }))
     ]
 };
-
